3f4782d7ccc252cebf095ddaf3dace8d859e6031
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-09-08 23:57:33 -0300

    initial

---
+++ src/app/components/nova-tranferencia/nova-tranferencia.components.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-nova-tranferencia',
+    templateUrl: './nova-tranferencia.components.html',
+    styleUrls: ['./nova-tranferencia.components.scss'],
+})
+export class NovaTranferenciaComponents{
+
+}
